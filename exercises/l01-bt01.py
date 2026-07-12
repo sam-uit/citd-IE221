ecab69b541f1fba3444486a2a5f8f06d438461ef
sam-uit <25410291@ms.uit.edu.vn>
2026-07-12 23:02:07 +0800

    Create l01-bt01.py

---
+++ exercises/l01-bt01.py
@@ -0,0 +1,4 @@
+# 2. Viết CT nhập thông tin từ bàn phím, sau đó in ra chuỗi, rồi kiểm tra kiểu dữ liệu nhập vào.
+# 3. Viết chương trình nhập vào hai string. Sau đó dùng `""` hoặc "`+`" trong `print()` để kết hai string đó và xuất ra màn hình.
+# 4. Viết lệnh nhập thông tin từ bàn phím, rồi xuất ra giá trị nhập, viết lệnh `input()` lồng trong lệnh `print()`.
+
