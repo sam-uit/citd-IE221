b30b63e6c232d5aa8f36d1d1a190219129e1c3e3
sam-uit <25410291@ms.uit.edu.vn>
2026-07-12 23:16:55 +0800

    Update l01-bt01.py
    [A] Nhập chuỗi, in chuỗi & kiểm tra kiểu dữ liệu.

--- exercises/l01-bt01.py
+++ exercises/l01-bt01.py
@@ -1,4 +1,10 @@
 # 2. Viết CT nhập thông tin từ bàn phím, sau đó in ra chuỗi, rồi kiểm tra kiểu dữ liệu nhập vào.
-# 3. Viết chương trình nhập vào hai string. Sau đó dùng `""` hoặc "`+`" trong `print()` để kết hai string đó và xuất ra màn hình.
-# 4. Viết lệnh nhập thông tin từ bàn phím, rồi xuất ra giá trị nhập, viết lệnh `input()` lồng trong lệnh `print()`.
 
+print("[A] Nhập chuỗi, in chuỗi & kiểm tra kiểu dữ liệu.")
+hovaten = input("[1] Vui lòng nhập họ và tên: ")
+print(f"[2] Thông tin bạn nhập vào là: {hovaten}")
+print("[3] Kiểu dữ liệu là: " + type(hovaten).__name__)
+
+# 3. Viết chương trình nhập vào hai string. Sau đó dùng `""` hoặc "`+`" trong `print()` để kết hai string đó và xuất ra màn hình.
+
+# 4. Viết lệnh nhập thông tin từ bàn phím, rồi xuất ra giá trị nhập, viết lệnh `input()` lồng trong lệnh `print()`.
