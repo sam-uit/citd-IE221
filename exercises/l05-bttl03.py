fb8f91cc02fc8dc3c6ced971ca6aee14af2505f6
sam-uit <25410291@ms.uit.edu.vn>
2026-08-09 10:13:40 +0800

    Create l05-bttl03.py

---
+++ exercises/l05-bttl03.py
@@ -0,0 +1,5 @@
+# Viết hàm tính căn bậc n từ một số thựuc.
+# Viết hàm tính giá trị bình phương của một số dương.
+# Viêt hàm kiểm tra một số nhập vào là số chẵn có giá trị âm. Đúng trả về True, sai trả về False.
+# Viết hàm kiểm tra một số nhập vào: nếu là số âm có giá trị lẻ thì trả về -1, nếu là số dương có giá trị chẵn thì trả về 1, trường hợp khác trả về 0.
+# Viết hàm kiểm tra giá trị nhập vào phải thuộc đoạn [-89, 90], nếu sai bắt nhập lại.
