24376df171a85cc5b560d5a1789c2f3a36f4958f
sam-uit <25410291@ms.uit.edu.vn>
2026-08-21 07:54:34 +0800

    Create main.py

---
+++ exercises/l06-bt03/my_package/geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# MSSV: 25410291
+# l06-bt03
+# my_package/geometry.py
+# Tính diện tích, chu vi hình vuông, hình tròn
+
+def dien_tich(canh):
+    return canh * canh
+
+def chu_vi(canh):
+    return 4 * canh
