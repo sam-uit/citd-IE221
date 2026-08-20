37104fcf8528f9fcc78cce5cf316eb4c02f4123f
sam-uit <25410291@ms.uit.edu.vn>
2026-08-21 07:51:37 +0800

    Create l06-bt02.py

---
+++ exercises/l06-bt02/l06-bt02.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# MSSV: 25410291
+# l06-bt02.py
+
+import math as m
+import random as rd
+
+# Tính căn bậc hai của 25
+print(f"Tính căn bậc hai của 25: {m.sqrt(25)}")
+
+# Tính cos của 60 độ (chuyển sang radian)
+print(f"Tính cos của 60 độ (radian): {m.cos(m.radians(60))}")
+
+# Sinh một số ngẫu nhiên từ 1 đến 100
+print(f"Sinh một số ngẫu nhiên từ 1 đến 100: {rd.randint(1, 100)}")
