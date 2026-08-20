23d20ade962e28ab83d587f05c167a5f1dae3f1c
sam-uit <25410291@ms.uit.edu.vn>
2026-08-21 07:59:14 +0800

    Update geometry.py

--- exercises/l06-bt03/my_package/geometry.py
+++ exercises/l06-bt03/my_package/geometry.py
@@ -4,8 +4,14 @@
 # my_package/geometry.py
 # Tính diện tích, chu vi hình vuông, hình tròn
 
-def dien_tich(canh):
-    return canh * canh
+def dien_tich(**kwargs):
+    if 'canh' in kwargs:
+        return kwargs['canh'] * kwargs['canh']
+    if 'ban_kinh' in kwargs:
+        return kwargs['ban_kinh'] * kwargs['ban_kinh'] * 3.14
 
-def chu_vi(canh):
-    return 4 * canh
+def chu_vi(**kwargs):
+    if 'canh' in kwargs:
+        return 4 * kwargs['canh']
+    if 'ban_kinh' in kwargs:
+        return 2 * kwargs['ban_kinh'] * 3.14
