69588955e8dc22c1ce6d2e027d1bec746ac0f3a6
sam-uit <25410291@ms.uit.edu.vn>
2026-08-21 07:48:10 +0800

    l06-bt01

---
+++ exercises/l06-bt01/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# main.py
+
+import random
+import math_utils as mu
+
+def main():
+    x = random.randint(0, 10)
+    y = random.randint(0, 10)
+    print(f"Test with x: {x}, y: {y}")
+    print(f"[i] sum: {mu.add(x, y)}")
+    print(f"[i] subtract: {mu.subtract(x, y)}")
+    print(f"[i] multiply: {mu.multiply(x, y)}")
+    try:
+        print(f"[i] divide: {mu.divide(x, y)}")
+    except ValueError as e:
+        print(f"[x] Error: {e}")
+
+if __name__ == "__main__":
+    main()
---
+++ exercises/l06-bt01/math_utils.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# math_utils.py
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
