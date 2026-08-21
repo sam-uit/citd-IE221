7ad5e9936ceb0e0dce9a6a71ace1c7e215623402
sam-uit <25410291@ms.uit.edu.vn>
2026-08-21 08:00:23 +0800

    Update converter.py

---
+++ exercises/l06-bt03/my_package/converter.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# MSSV: 25410291
+# l06-bt03
+# my_package/converter.py
+# Đổi giá trị cm -> m, m -> km
+
+def cm_to_m(cm: float) -> float:
+    return cm / 100
+
+def m_to_km(m: float) -> float:
+    return m / 1000
