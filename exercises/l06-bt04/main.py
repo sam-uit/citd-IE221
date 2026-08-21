ded143458b55be2dc63ad75157f2bbe24c477460
sam-uit <25410291@ms.uit.edu.vn>
2026-08-21 08:18:17 +0800

    Update main.py

---
+++ exercises/l06-bt04/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# MSSV: 25410291
+# l06-bt04
+# main.py
+
+from Game.Image import open
+from Game.Sound import play
+
+print("[i] Đang gọi open_image()")
+# open image
+open.open_image()
+
+print()
+
+print("[i] Đang gọi play_sound()")
+# play sound
+play.play_sound()
